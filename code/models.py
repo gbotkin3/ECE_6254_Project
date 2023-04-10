8d2e2f57839fdbe5638ceb83cd58cbfd6641855f
gbotkin1999@gmail.com <gbotkin1999@gmail.org>
2023-04-10 11:43:09 -0400

    Merged Branches

---
+++ code/models.py
@@ -0,0 +1,125 @@
+from dataloader import *
+from sklearn.svm import LinearSVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.gaussian_process.kernels import RBF
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import accuracy_score, balanced_accuracy_score, mean_squared_error, roc_auc_score
+from sklearn.calibration import CalibratedClassifierCV
+
+import numpy as np 
+
+# Loading Datasets
+def loadAllDatasets():
+    drug = LoadDataset("drug200.csv")
+    drug = GetDummies(drug, columns = ["Sex", "BP", "Cholesterol"], prefix = None)
+    # Move Drug column to the end
+    target_col = drug.pop("Drug")
+    drug.insert(len(drug.columns), "Drug", target_col)
+
+    iris = LoadDataset("iris.csv")
+    iris.pop("Id") # Remove Id Column
+
+    seeds = LoadDataset("seeds.csv")
+    return(drug, iris, seeds)
+
+def splitFeaturesTarget(drug, iris, seeds):
+    drug_features = drug[drug.columns[0:len(drug.columns) - 1]]
+    drug_target = drug[drug.columns[-1]]
+
+    iris_features = iris[iris.columns[0:len(iris.columns) - 1]]
+    iris_target = iris[iris.columns[-1]]
+
+    seeds_features = seeds[seeds.columns[0:len(seeds.columns) - 1]]
+    seeds_target = seeds[seeds.columns[-1]]
+    features = [drug_features, iris_features, seeds_features]
+    targets = [drug_target, iris_target, seeds_target]
+    return(features, targets)
+
+def applyDecisionTree(Xtrain, Ytrain, max_depth, min_samples_split, min_samples_leaf, criterion):
+    tree_clf = DecisionTreeClassifier(max_depth=max_depth, min_samples_split=min_samples_split, min_samples_leaf=min_samples_leaf, criterion=criterion)
+    tree_clf = tree_clf.fit(Xtrain, Ytrain)
+    return tree_clf
+
+def applyLinearSVC(Xtrain, Ytrain):
+    linearSVC_clf = CalibratedClassifierCV(LinearSVC())
+    linearSVC_clf = linearSVC_clf.fit(Xtrain, Ytrain)
+    return linearSVC_clf
+
+def applyGP(Xtrain, Ytrain, kernel):
+    gp_clf = GaussianProcessClassifier(kernel=kernel)
+    gp_clf = gp_clf.fit(Xtrain, Ytrain)
+    return gp_clf
+
+def applyKNN(Xtrain, Ytrain, n_neighbors):
+    knn_clf = KNeighborsClassifier(n_neighbors=n_neighbors)
+    knn_clf = knn_clf.fit(Xtrain, Ytrain)
+    return knn_clf
+
+def DecisionTreeTuning(Xtrain, Ytrain, Xtest, Ytest):
+    param_grid = {
+        "max_depth": [3,5,10,15,20],
+        "min_samples_split": [2,5,7,10],
+        "min_samples_leaf": [1,2,5],
+        "criterion": ['gini', 'entropy']
+    }
+
+    clf = DecisionTreeClassifier(random_state=42)
+    grid_cv = GridSearchCV(clf, param_grid, scoring="roc_auc_ovr", n_jobs=-1, cv=3).fit(Xtrain, Ytrain)
+
+    print("TREE: Param for GS", grid_cv.best_params_)
+    print("TREE: CV score for GS", grid_cv.best_score_)
+    print("TREE: Train AUC ROC Score for GS: ", roc_auc_score(Ytrain, grid_cv.predict_proba(Xtrain), multi_class='ovr'))
+    print("TREE: Test AUC ROC Score for GS: ", roc_auc_score(Ytest, grid_cv.predict_proba(Xtest), multi_class='ovr'))
+    return grid_cv.best_params_
+
+def LinearSVCTuning(Xtrain, Ytrain, Xtest, Ytest):
+    param_grid = {
+        "C": [1,10,100,1000],
+        "tol": [1,0.1,0.01,0.001,0.0001, 0.00001],
+        "loss": ['squared_hinge'],
+        "penalty": ['l2']
+    }
+
+    clf = CalibratedClassifierCV(LinearSVC(random_state=42))
+    grid_cv = GridSearchCV(clf, param_grid, scoring="roc_auc_ovr", n_jobs=-1, cv=3).fit(Xtrain, Ytrain)
+
+    print("SVC: Param for GS", grid_cv.best_params_)
+    print("SVC: CV score for GS", grid_cv.best_score_)
+    print("SVC: Train AUC ROC Score for GS: ", roc_auc_score(Ytrain, grid_cv.predict(Xtrain), multi_class='ovr'))
+    print("SVC: Test AUC ROC Score for GS: ", roc_auc_score(Ytest, grid_cv.predict(Xtest), multi_class='ovr'))
+
+def gpTuning(Xtrain, Ytrain, Xtest, Ytest):
+    param_grid = {
+        "kernel": [RBF(l) for l in np.logspace(-1, 0, 1, 2)],
+    }
+
+    clf = GaussianProcessClassifier(random_state=42)
+    grid_cv = GridSearchCV(clf, param_grid, scoring="roc_auc_ovr", n_jobs=-1, cv=3).fit(Xtrain, Ytrain)
+
+    print("GP: Param for GS", grid_cv.best_params_)
+    print("GP: CV score for GS", grid_cv.best_score_)
+    print("GP: Train AUC ROC Score for GS: ", roc_auc_score(Ytrain, grid_cv.predict_proba(Xtrain), multi_class='ovr'))
+    print("GP: Test AUC ROC Score for GS: ", roc_auc_score(Ytest, grid_cv.predict_proba(Xtest), multi_class='ovr'))
+    return grid_cv.best_params_
+
+def KNNTuning(Xtrain, Ytrain, Xtest, Ytest):
+    param_grid = {
+        "n_neighbors": [3, 5, 7, 10, 13, 15, 17, 20, 23, 25, 27, 30],
+    }
+
+    clf = KNeighborsClassifier()
+    grid_cv = GridSearchCV(clf, param_grid, scoring="roc_auc_ovr", n_jobs=-1, cv=3).fit(Xtrain, Ytrain)
+
+    print("KNN: Param for GS", grid_cv.best_params_)
+    print("KNN: CV score for GS", grid_cv.best_score_)
+    print("KNN: Train AUC ROC Score for GS: ", roc_auc_score(Ytrain, grid_cv.predict_proba(Xtrain), multi_class='ovr'))
+    print("KNN: Test AUC ROC Score for GS: ", roc_auc_score(Ytest, grid_cv.predict_proba(Xtest), multi_class='ovr'))
+    return grid_cv.best_params_
+
+
+def testModel(model, Xtest, Ytest):
+    y_pred = model.predict(Xtest)
+    acc = accuracy_score(y_true = Ytest, y_pred = y_pred)
+    return acc
---
+++ code/visualization.py
@@ -0,0 +1,109 @@
+# Methods for Visualizing Data
+
+import numpy as np
+import pandas as pd
+import seaborn as sb
+import matplotlib.pyplot as plt
+
+# Function that creates a 3D scatterplot using the first 3 features (first 3 columns for all rows)
+# of the data that is passed in (may need to reduce dimensionality to 3 components with PCA or other methods)
+# All of our datasets have more than 3 features, so we use PCA to reduce the data to 3 principal components to use
+# for the scatterplot.
+
+# data_labels is the label of each sample (row) of the data used to color the points according to their labels
+
+# show_plot is a flag that is used to specify whether to show a plot or not.
+
+# dataset_name is used to distinguish between different plots
+def scatter_plot_all(data, data_labels, show_plot, dataset_name):
+
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+
+    X = np.zeros(len(data))
+    Y = np.zeros(len(data))
+    Z = np.zeros(len(data))
+
+    for i in range(len(data)):
+        X[i] = data[i][0]
+        Y[i] = data[i][1]
+        Z[i] = data[i][2]
+
+    ax.scatter(X, Y, Z, c=data_labels) 
+
+    ax.set_xlabel('Component 0')
+    ax.set_ylabel('Component 1')
+    ax.set_zlabel('Component 2')
+
+    plt.title(dataset_name + " Dataset 3D Scatterplot")
+
+    # Save this scatterplot figure
+    plt.savefig("../results/figures/" + dataset_name + "_3D_scatterplot.png")
+
+    if show_plot:
+        plt.show()
+
+    return
+
+
+# This function creates a KDE plots for a passed in pandas dataframe
+# dataframe is the pandas dataframe of the data we want to use to generate pairplots
+# show_plot is the flag that specifies whether to show the plot or not
+# also include the name associated with the dataset so that we can save the plots and title it.
+def kde_map_all(dataframe, show_plot, dataset_name, xaxis_label=None): ## Need to Fix Labels
+
+    #data_coc = np.concatenate((data, data_labels), axis = 1)
+    #dataframe = pd.DataFrame(data_coc, columns=["Component 0", "Component 1", "Component 2", "Label"])
+
+    g = sb.FacetGrid(dataframe, col = dataframe.columns.values.tolist()[-1])
+    #g.map(sb.kdeplot, "Component 0", color = 'r', legend = True)
+    #g.map(sb.kdeplot, "Component 1", color = 'g', legend = True)
+    #g.map(sb.kdeplot, "Component 2", color = 'b', legend = True)
+    colors = sb.color_palette()
+    for i, col in enumerate(dataframe.columns.values.tolist()[:-1]):
+        # if col == dataframe.columns.values.tolist()[-1]
+        g.map(sb.kdeplot, col, color = colors[i], legend = True, label=col)
+    # print(dataframe.columns.values.tolist()[:-1])
+    # g.map_dataframe(sb.kdeplot, dataframe.columns.values.tolist()[:-1], legend=True)
+
+    if xaxis_label is not None:
+        g.set_xlabels(xaxis_label)
+
+
+    g.add_legend()
+    
+    plt.savefig("../results/figures/" + dataset_name + "_kde.png")
+
+    if show_plot:
+        plt.show()
+
+    return
+
+# Instead of passing in numpy rows and columns, let's pass in the pandas dataframe so that we can use Seaborn for pairplots.
+#def pairplot_all(data, data_labels, show_plot):
+
+# This function creates a plot of all possible pair plots
+# The diagonal plots show a plot of the univariate distribution (marginal distribution of the data in each column)
+# dataframe is the pandas dataframe of the data we want to use to generate pairplots
+# show_plot is the flag that specifies whether to show the plot or not
+# also include the name associated with the dataset so that we can save the plots and title it.
+def pairplot_all(dataframe, show_plot, dataset_name):
+
+    #data_coc = np.concatenate((data, data_labels), axis = 1)
+    #dataframe = pd.DataFrame(data_coc, columns=["Component 0", "Component 1", "Component 2", "Component 3", "Label"])
+
+    # Grab the last column, which is the label column to color the samples (points) (distributions) based on their label
+    # Use seaborn to generate all the pairplots for any 2 features and the univariate distributions for all features
+    plots = sb.pairplot(dataframe, hue = dataframe.columns.values.tolist()[-1])
+
+    # save the figure of pair plots.
+    plt.savefig("../results/figures/" + dataset_name + "_pairplots.png")  
+    
+    # Title the plot based on the name of the dataset
+    #plt.title(dataset_name + " Pair Plots")
+    #plots.fig.suptitle(dataset_name + " Pair Plots", y=1.08)
+
+    if show_plot:
+        plt.show()
+
+    return
