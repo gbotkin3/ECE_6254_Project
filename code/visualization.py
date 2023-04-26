f1fad7c347ca431d349aec510d2b8d3b3348d6cc
briceminix <83382588+briceminix@users.noreply.github.com>
2023-04-25 22:40:55 -0400

    Update visualization.py
    Updated visualization to produce better plots for report.

--- code/visualization.py
+++ code/visualization.py
@@ -15,6 +15,8 @@ import matplotlib.pyplot as plt
 # show_plot is a flag that is used to specify whether to show a plot or not.
 
 # dataset_name is used to distinguish between different plots
+
+
 def scatter_plot_all(data, data_labels, show_plot, dataset_name):
 
     fig = plt.figure()
@@ -29,13 +31,14 @@ def scatter_plot_all(data, data_labels, show_plot, dataset_name):
         Y[i] = data[i][1]
         Z[i] = data[i][2]
 
-    ax.scatter(X, Y, Z, c=data_labels) 
+    ax.scatter(X, Y, Z, c=data_labels)
 
     ax.set_xlabel('Component 0')
     ax.set_ylabel('Component 1')
     ax.set_zlabel('Component 2')
 
-    plt.title(dataset_name + " Dataset 3D Scatterplot")
+    # If we want to add the title to the 3D scatterplot.
+    #plt.title(dataset_name + " Dataset 3D Scatterplot")
 
     # Save this scatterplot figure
     plt.savefig("../results/figures/" + dataset_name + "_3D_scatterplot.png")
@@ -50,28 +53,37 @@ def scatter_plot_all(data, data_labels, show_plot, dataset_name):
 # dataframe is the pandas dataframe of the data we want to use to generate pairplots
 # show_plot is the flag that specifies whether to show the plot or not
 # also include the name associated with the dataset so that we can save the plots and title it.
-def kde_map_all(dataframe, show_plot, dataset_name, xaxis_label=None): ## Need to Fix Labels
+def kde_map_all(dataframe, show_plot, dataset_name, xaxis_label=None):  # Need to Fix Labels
 
     #data_coc = np.concatenate((data, data_labels), axis = 1)
     #dataframe = pd.DataFrame(data_coc, columns=["Component 0", "Component 1", "Component 2", "Label"])
+    sb.set_style(rc={'xtick.direction': 'in', 'ytick.direction': 'in', 'xtick.top': True, 'ytick.right': True})
 
-    g = sb.FacetGrid(dataframe, col = dataframe.columns.values.tolist()[-1])
+    g = sb.FacetGrid(dataframe, col=dataframe.columns.values.tolist()[-1])
     #g.map(sb.kdeplot, "Component 0", color = 'r', legend = True)
     #g.map(sb.kdeplot, "Component 1", color = 'g', legend = True)
     #g.map(sb.kdeplot, "Component 2", color = 'b', legend = True)
     colors = sb.color_palette()
     for i, col in enumerate(dataframe.columns.values.tolist()[:-1]):
         # if col == dataframe.columns.values.tolist()[-1]
-        g.map(sb.kdeplot, col, color = colors[i], legend = True, label=col)
+        g.map(sb.kdeplot, col, color=colors[i], legend=True, label=col)
     # print(dataframe.columns.values.tolist()[:-1])
+    for ax in g.axes.flat:
+        #print(ax)
+        sb.despine(left=False, right=False, bottom=False, top=False, ax=ax)
+        #ax.xaxis.set_ticks_position('top')
+        ax.xaxis.set_ticks_position('both')
+        #ax.yaxis.set_ticks_position('right')
+        ax.yaxis.set_ticks_position('both')
+        plt.setp(ax.yaxis.get_ticklabels(), visible=ax.is_first_col())
+        plt.setp(ax.xaxis.get_ticklabels(), visible=ax.is_last_row())
     # g.map_dataframe(sb.kdeplot, dataframe.columns.values.tolist()[:-1], legend=True)
 
     if xaxis_label is not None:
         g.set_xlabels(xaxis_label)
 
-
     g.add_legend()
-    
+
     plt.savefig("../results/figures/" + dataset_name + "_kde.png")
 
     if show_plot:
@@ -80,25 +92,39 @@ def kde_map_all(dataframe, show_plot, dataset_name, xaxis_label=None): ## Need t
     return
 
 # Instead of passing in numpy rows and columns, let's pass in the pandas dataframe so that we can use Seaborn for pairplots.
-#def pairplot_all(data, data_labels, show_plot):
+# def pairplot_all(data, data_labels, show_plot):
 
 # This function creates a plot of all possible pair plots
 # The diagonal plots show a plot of the univariate distribution (marginal distribution of the data in each column)
 # dataframe is the pandas dataframe of the data we want to use to generate pairplots
 # show_plot is the flag that specifies whether to show the plot or not
 # also include the name associated with the dataset so that we can save the plots and title it.
+
+
 def pairplot_all(dataframe, show_plot, dataset_name):
 
     #data_coc = np.concatenate((data, data_labels), axis = 1)
     #dataframe = pd.DataFrame(data_coc, columns=["Component 0", "Component 1", "Component 2", "Component 3", "Label"])
 
+    sb.set_style(rc={'xtick.direction': 'in', 'ytick.direction': 'in', 'xtick.top': True, 'ytick.right': True})
+
     # Grab the last column, which is the label column to color the samples (points) (distributions) based on their label
     # Use seaborn to generate all the pairplots for any 2 features and the univariate distributions for all features
-    plots = sb.pairplot(dataframe, hue = dataframe.columns.values.tolist()[-1])
-
+    plots = sb.pairplot(dataframe, hue=dataframe.columns.values.tolist()[-1], grid_kws=dict(diag_sharey=False))
+    # plots.box(on=True)
+    # plt.box(on=True)
+    for ax in plots.axes.flat:
+        #print(ax)
+        sb.despine(left=False, right=False, bottom=False, top=False, ax=ax)
+        #ax.xaxis.set_ticks_position('top')
+        ax.xaxis.set_ticks_position('both')
+        #ax.yaxis.set_ticks_position('right')
+        ax.yaxis.set_ticks_position('both')
+        plt.setp(ax.yaxis.get_ticklabels(), visible=ax.is_first_col())
+        plt.setp(ax.xaxis.get_ticklabels(), visible=ax.is_last_row())
     # save the figure of pair plots.
-    plt.savefig("../results/figures/" + dataset_name + "_pairplots.png")  
-    
+    plt.savefig("../results/figures/" + dataset_name + "_pairplots.png")
+
     # Title the plot based on the name of the dataset
     #plt.title(dataset_name + " Pair Plots")
     #plots.fig.suptitle(dataset_name + " Pair Plots", y=1.08)
@@ -108,7 +134,7 @@ def pairplot_all(dataframe, show_plot, dataset_name):
 
     return
 
-def heatmap(): ## ASK PROF
+
+def heatmap():  # ASK PROF
 
     return
-
